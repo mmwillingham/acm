5cca44be924c5a0530e62f88f7f7f26e7f38d4d8
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2023-12-18 10:40:06 -0500

    Update addusers.sh

--- manual-stuff-not-necessary/addusers.sh
+++ manual-stuff-not-necessary/addusers.sh
@@ -3,13 +3,17 @@
 # Reference: /home/martin/Documents/RedHat/Training/do480-2.4-student-guide.pdf
 # Reference: https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.5
 
-# Create htpasswd users 
-touch htpasswd
-htpasswd -Bb htpasswd tom redhat
-htpasswd -Bb htpasswd mmw redhat
-htpasswd -Bb htpasswd martin redhat
+# Prereq
+sudo dnf install httpd-utils
 
-oc --user=admin create secret generic htpasswd --from-file=htpasswd -n openshift-config
+# Create htpasswd users 
+touch /tmp/htpasswd
+htpasswd -Bb /tmp/htpasswd tom redhat
+htpasswd -Bb /tmp/htpasswd mmw redhat
+htpasswd -Bb /tmp/htpasswd martin redhat
+
+#oc --user=admin create secret generic htpasswd --from-file=/tmp/htpasswd -n openshift-config
+oc create secret generic htpass-secret --from-file=htpasswd=/tmp/htpasswd -n openshift-config
 oc replace -f - <<EOF
 apiVersion: config.openshift.io/v1
 kind: OAuth
@@ -24,7 +28,9 @@ spec:
       fileData:
         name: htpasswd
 EOF
+
 oc adm groups new mylocaladmins
 oc adm groups add-users mylocaladmins tom mmw martin
 oc adm policy add-cluster-role-to-group cluster-admin mylocaladmins
+
 
